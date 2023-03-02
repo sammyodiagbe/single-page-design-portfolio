0ad9e6e4b91f2867eb448ca3f66688f038b7795a
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-01 20:59:43 -0600

    structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Logo from "./components/logo";
+
 function App() {
-  return <div className="single-page-portfolio"></div>;
+  return (
+    <div className="single-page-portfolio">
+      <nav className="navigation">
+        <Logo />
+        <a href="#consultant" className="btn">
+          Free Consultation
+        </a>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/right_arrow.js
@@ -0,0 +1,12 @@
+const RightArrow = () => {
+  return (
+    <svg viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill="#FFF"
+        d="m6.808 16 8-8-8-8-1.687 1.687 5.121 5.12H.001v2.386h10.241l-5.121 5.12z"
+      />
+    </svg>
+  );
+};
+
+export default RightArrow;
